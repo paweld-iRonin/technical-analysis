41da815e255427f05069073100f4441c1cefb985
Paweł Dąbrowski <pawel.dabrowski@ironin.pl>
2020-06-19 13:46:43 +0200

    add debug

--- lib/technical_analysis/helpers/stock_calculation.rb
+++ lib/technical_analysis/helpers/stock_calculation.rb
@@ -16,7 +16,7 @@ module TechnicalAnalysis
     def self.ema(current_value, data, period, prev_value)
       if prev_value.nil?
         #ArrayHelper.average(data)
-        binding.pry
+        current_value
       else
         (current_value - prev_value) * (2.0 / (period + 1.0)) + prev_value
       end
